1c8e9220583d52cf4fcf80143289f4a356b8f1ad
sihyeon <sihyeon.dev@gmail.com>
2019-04-23 13:17:53 +0900

    test : Check UART func

--- src/esp8266/boot.py
+++ src/esp8266/boot.py
@@ -5,15 +5,27 @@ import uos, machine
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 import webrepl
+
+
 webrepl.start()
 gc.collect()
 
-def do_connect(ssid, pswd):
+
+def connect_wlan(ssid, pswd):
     import network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
+        print('connecting to network...')
         wlan.connect(ssid, pswd)
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
+
+
+def init_servo(pin):
+    servo = machine.PWM(machine.Pin(pin), freq=50)
+
+    return servo
+
+
