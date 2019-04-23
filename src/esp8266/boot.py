02f8e5fe3d746c3b872b50af74bc0eae440a3e33
Sihyeon Lee <sihyeon.dev@gmail.com>
2019-04-23 23:02:25 +0900

    feat : Control robotarm with controller

--- src/esp8266/boot.py
+++ src/esp8266/boot.py
@@ -2,30 +2,17 @@
 #import esp
 #esp.osdebug(None)
 import uos, machine
-#uos.dupterm(None, 1) # disable REPL on UART(0)
+uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 import webrepl
-
-
 webrepl.start()
 gc.collect()
-
-
 def connect_wlan(ssid, pswd):
     import network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
-        print('connecting to network...')
         wlan.connect(ssid, pswd)
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
-
-
-def init_servo(pin):
-    servo = machine.PWM(machine.Pin(pin), freq=50)
-
-    return servo
-
-
